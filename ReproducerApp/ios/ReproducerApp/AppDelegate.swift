4eb3ceb7d1549f77004033a5d498ce6d27867b9d
Chetan sharma <69357055+IAMDeveloperChetanSharma@users.noreply.github.com>
2025-07-23 13:38:31 +0530

    Update AppDelegate.swift

--- ReproducerApp/ios/ReproducerApp/AppDelegate.swift
+++ ReproducerApp/ios/ReproducerApp/AppDelegate.swift
@@ -2,47 +2,44 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Firebase
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+class AppDelegate: RCTAppDelegate {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    self.moduleName = "cultureapp"
+    self.dependencyProvider = RCTAppDependencyProvider()
 
-  var reactNativeDelegate: ReactNativeDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
+    #if JS_LOGGING
+    // Enable console.log logging in macOS Console App for Release builds
+    RCTSetLogThreshold(.trace)
+    RCTSetLogFunction { level, source, fileName, lineNumber, message in
+      NSLog("%@", RCTFormatLog(nil, level, nil, nil, message))
+    }
+    #endif
 
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-  ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = RCTReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
-
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "ReproducerApp",
-      in: window,
-      launchOptions: launchOptions
-    )
-
-    return true
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
 
-class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
+   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
-
+ 
   override func bundleURL() -> URL? {
-#if DEBUG
+  #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
+    #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+  #endif
+  }
+
+  // Adds Deeplinking support
+  override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    return RCTLinkingManager.application(application, open: url, options: options)
+  }
+
+  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 }
